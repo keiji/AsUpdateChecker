b2b84bbf6a01796dec0f20453f7c2e9258acf510
Keiji Ariyama <keiji_ariyama@c-lis.co.jp>
2015-11-20 21:21:32 +0900

    XMLファイルを取得した。

--- app/src/main/java/io/keiji/asupdatechecker/Endpoint.java
+++ app/src/main/java/io/keiji/asupdatechecker/Endpoint.java
@@ -1,29 +1,44 @@
 package io.keiji.asupdatechecker;
 
+import android.util.Log;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import javax.net.ssl.HttpsURLConnection;
 
 public class Endpoint {
+    private static final String TAG = Endpoint.class.getSimpleName();
 
     private static final String ENDPOINT_URL = "https://dl.google.com/android/studio/patches/updates.xml";
 
     public static final String getUpdateState() throws IOException {
+        StringBuffer sb = new StringBuffer();
 
         HttpsURLConnection httpsURLConnection = null;
 
         try {
             URL url = new URL(ENDPOINT_URL);
             httpsURLConnection = (HttpsURLConnection) url.openConnection();
-            return httpsURLConnection.getContentEncoding();
+            httpsURLConnection.connect();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(httpsURLConnection.getInputStream()));
+
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
         } catch (MalformedURLException e) {
+            Log.e(TAG, "MalformedURLException", e);
+
         } finally {
             if (httpsURLConnection != null) {
                 httpsURLConnection.disconnect();
             }
         }
-        return null;
+        return sb.toString();
     }
 }
--- app/src/main/java/io/keiji/asupdatechecker/MainActivity.java
+++ app/src/main/java/io/keiji/asupdatechecker/MainActivity.java
@@ -1,7 +1,12 @@
 package io.keiji.asupdatechecker;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,6 +15,48 @@ import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final int LOADER_ID = 0x01;
+
+    public static class CHeckUpdateLoader extends AsyncTaskLoader<String> {
+
+        public CHeckUpdateLoader(Context context) {
+            super(context);
+        }
+
+        @Override
+        public String loadInBackground() {
+            String result = null;
+            try {
+                result = Endpoint.getUpdateState();
+            } catch (IOException e) {
+                Log.e(TAG, "IOException", e);
+            }
+
+            return result;
+        }
+    }
+
+    private final LoaderManager.LoaderCallbacks<String> mLoaderCallback = new LoaderManager.LoaderCallbacks<String>() {
+        @Override
+        public Loader<String> onCreateLoader(int id, Bundle args) {
+            CHeckUpdateLoader loader = new CHeckUpdateLoader(MainActivity.this);
+            loader.forceLoad();
+            return loader;
+        }
+
+        @Override
+        public void onLoadFinished(Loader<String> loader, String data) {
+            mState.setText(data);
+
+        }
+
+        @Override
+        public void onLoaderReset(Loader<String> loader) {
+
+        }
+    };
 
     private TextView mState;
     private Button mCheckUpdate;
@@ -21,7 +68,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mState = (TextView) findViewById(R.id.tv_state);
         mCheckUpdate = (Button) findViewById(R.id.btn_check);
-
         mCheckUpdate.setOnClickListener(this);
     }
 
@@ -35,12 +81,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void checkUpdate() {
-
-        try {
-            String result = Endpoint.getUpdateState();
-            mState.setText(result);
-        } catch (IOException e) {
-        }
-
+        getSupportLoaderManager().restartLoader(LOADER_ID, null, mLoaderCallback);
     }
 }
